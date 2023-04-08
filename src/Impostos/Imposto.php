67029ffa5730069f17856a916cd2e542765b4ce6
Marcelo Alves <marcelonralves@gmail.com>
2023-04-08 18:07:07 -0300

    feat: decorator

--- src/Impostos/Imposto.php
+++ src/Impostos/Imposto.php
@@ -4,7 +4,21 @@ namespace Alura\DesignPattern\Impostos;
 
 use Alura\DesignPattern\Orcamento;
 
-interface Imposto
+abstract class Imposto
 {
-    public function calculaImposto(Orcamento $orcamento): float;
+    public function __construct(private readonly ?Imposto $outroImposto = null)
+    {
+    }
+
+    abstract protected function realizaCalculoEspecifico(Orcamento $orcamento): float;
+
+    public function calculaImposto(Orcamento $orcamento): float|int
+    {
+        return $this->realizaCalculoEspecifico($orcamento) + $this->realizaCalculoDeOutroImposto($orcamento);
+    }
+
+    private function realizaCalculoDeOutroImposto(Orcamento $orcamento): float|int
+    {
+        return $this->outroImposto === null ? 0 : $this->outroImposto->calculaImposto($orcamento);
+    }
 }
