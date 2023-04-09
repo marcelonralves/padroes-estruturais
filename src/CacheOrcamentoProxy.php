edfd86d477e3a750925b23da1b3aaebc5ad5dfb3
Marcelo Alves <marcelonralves@gmail.com>
2023-04-09 03:18:43 -0300

    feat: cache

---
+++ src/CacheOrcamentoProxy.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Alura\DesignPattern;
+
+class CacheOrcamentoProxy extends Orcamento
+{
+    private float $valorCache = 0;
+
+    public function __construct(private Orcamento $orcamento)
+    {
+    }
+
+    public function addItem(Orcavel $orcavel)
+    {
+        throw new \DomainException('Não é possível adicionar item a orçamento cacheado');
+    }
+
+    public function valor(): float
+    {
+        if($this->valorCache == 0) {
+            $this->valorCache = $this->orcamento->valor();
+        }
+
+        return $this->valorCache;
+    }
+}
